68b0f8d2942a6d3debb015bce16c24d6341bd35f
Izanalgit <izanalvarez@gmail.com>
2024-07-20 15:06:54 +0200

    feat : get fetch hoock

---
+++ src/hooks/useFetchGET.jsx
@@ -0,0 +1,38 @@
+import { useState , useEffect } from "react";
+import axios from "axios";
+
+const useFetchGET = (url) => {
+    
+    const [data, setData] = useState(null);
+    const [status, setStatus] = useState(false);
+    const [errMsg, setErrMsg] = useState(null);
+   
+    useEffect(()=>{
+
+        axios.get(url)
+            .then((response) => {
+                setData(response);
+            })
+            .catch((error) => {
+                if (error.response) {
+                    setErrMsg(`Error estado : ${error.response.status}`);
+                } else if (error.request) {
+                    setErrMsg(`No responde`);
+                    console.log(error.request);
+                } else {
+                    setErrMsg(`Error desconocido`);
+                    console.log(error.message);
+                }
+            })
+            .finally(() => {
+                setStatus(true);
+            })
+
+    },[url]);
+
+
+    return {data,status,errMsg};
+
+}
+
+export default useFetchGET;
