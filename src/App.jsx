3228e5b79ccdd4d2d9469cc78c1dec75dbb2be74
Izanalgit <izanalvarez@gmail.com>
2024-07-22 22:10:43 +0200

    feat : health fetch

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,27 @@ import AppRoutes from "./routes/AppRoutes";
 import { useApp } from "./context/AppContext";
 import useFetchGET from "./hooks/useFetchGET";
 
-const API_DATA_URL = "http://localhost:8080/public/projects";
+const API = "http://localhost:8080"
+const API_HEALTH = API +"/public/health";
+const API_DATA = API + "/public/projects";
 
 const App = () => {
 
     const {getProjectsData} = useApp();
-    const {data,status,errMsg} = useFetchGET(API_DATA_URL);  
+
+    const {data:health,status:pingAPI} = useFetchGET(API_HEALTH);
+    const {data,status,errMsg} = useFetchGET(API_DATA);
+
+    const sleepApiMsg = "La API esta invernando, puede que algun contenido tarde en cargar...";
 
     useEffect(()=>{
-        
+        if(!health && pingAPI) alert(sleepApiMsg)
+    },[pingAPI]);
+      
+    useEffect(()=>{
         if(status && !errMsg)
             getProjectsData(data.data);
-
-    },[status])
+    },[status]);
 
     return (
         <>
